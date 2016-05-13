40450a7a7627a8658208139845610e2ee30ca963
qiushaofan <517611129@qq.com>
2016-05-13 21:20:40 +0800

    第一次提交

---
+++ KiteNewsSwift/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  KiteNewsSwift
+//
+//  Created by 邱少藩 on 16/5/12.
+//  Copyright © 2016年 邱少藩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnLogin:UIButton!
+    @IBAction func onLogin(){
+       btnLogin.backgroundColor=UIColor.blackColor()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
