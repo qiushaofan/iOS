bcae2f7fd706d3ef12408d0da1ea6dbf413371a8
qiushaofan <517611129@qq.com>
2016-05-22 21:39:30 +0800

    增加网络加载图片，网络添加滚动图片，scrollview控件初步使用

--- KiteNewsSwift/MainViewController.swift
+++ KiteNewsSwift/MainViewController.swift
@@ -12,24 +12,52 @@ import SDCycleScrollView
 class MainViewController: UIViewController,SDCycleScrollViewDelegate {
     
     var sdCycleScrollView:SDCycleScrollView!
+    var sdCycleScrollView2:SDCycleScrollView!
+    @IBOutlet weak var scrollview:UIScrollView!
+    
     let imageArray:NSArray! = ["wallpaper_cat.jpg","wallpaper_mom.jpg","wallpaper_tree.jpg"]
+    
+    let urlImageArray:NSArray! = [
+        "http://www.henansc.com/uploadfile/2015/0210/20150210104804382.jpg",
+        
+        "http://www.henansc.com/uploadfile/2015/0212/20150212081452412.jpg",
+        
+        "http://www.henansc.com/uploadfile/2015/0212/20150212081841462.jpg",
+        
+        "http://www.henansc.com/uploadfile/2015/0210/20150210110611181.jpg",
+        
+        "http://www.henansc.com/uploadfile/2015/0210/20150210105956264.jpg"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let frame:CGRect! = CGRect.init(x: 0, y: 20, width: UIScreen.mainScreen().bounds.width, height: 200)
-        let firstImage:UIImage! = UIImage.init(named: "wallpaper_cat.jpg")
+        let frame1:CGRect! = CGRect.init(x: 0, y: 230, width: UIScreen.mainScreen().bounds.width, height: 200)
         
-        
-        
-        
-//        sdCycleScrollView = SDCycleScrollView.init(frame: frame, delegate: self, placeholderImage: firstImage)
-        sdCycleScrollView=SDCycleScrollView.init(frame: frame, imageNamesGroup: imageArray as [AnyObject])
+        sdCycleScrollView = SDCycleScrollView.init(frame: frame, imageNamesGroup: imageArray as [AnyObject])
+        sdCycleScrollView2=SDCycleScrollView.init(frame: frame1, imageURLStringsGroup: urlImageArray as [AnyObject])
         
         self.view.addSubview(sdCycleScrollView)
+        self.view.addSubview(sdCycleScrollView2)
         
-        //        let imageView=UIImageView(image:UIImage(named:"wallpaper_cat.jpg"))
-        //        imageView.frame=CGRectMake(10,10,100,100)
-        //        self.view.addSubview(imageView)
+        
+        
+        ImageLoader.sharedLoader.imageForUrl(urlImageArray[0] as! String, completionHandler:{(image:UIImage?,url:String) in
+            let imageView:UIImageView!=UIImageView.init(frame: CGRect.init(x: 0, y: 0, width: UIScreen.mainScreen().bounds.width, height: 400))
+            imageView.image=image
+            self.scrollview.addSubview(imageView)
+            
+            
+            //self.scrollview.bounces=false
+            self.scrollview.contentSize=CGSize.init(width: 1000, height: 2000)
+            self.scrollview.contentOffset = CGPoint(x: 0, y: 0)
+        })
+        
+        
+    }
+    
+    func scrollViewCanScroll()
+    {
         
     }
     
